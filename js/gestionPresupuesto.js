4c1e57eafb0fd94bed292c8b9181a45ce561c2c2
Ekaterina Vostrikova <ekavos@alu.edu.gva.es>
2024-10-17 19:35:49 +0200

    Fundamentos de JS 2: Crear variables globales; Crear funciones vacias; Añadir las funciones a export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,4 +1,6 @@
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 let comprobarPositivo = (numero) => typeof numero === "number" && numero >= 0;
 
@@ -11,6 +13,12 @@ let actualizarPresupuesto = (valor) => {
 };
 
 let mostrarPresupuesto = () => `Tu presupuesto actual es de ${presupuesto} €`;
+
+function listarGastos() {}
+function anyadirGasto() {}
+function borrarGasto() {}
+function calcularTotalGastos() {}
+function calcularBalance() {}
 
 function CrearGasto(descripcion, valor) {
   this.descripcion = descripcion;
@@ -31,4 +39,13 @@ function CrearGasto(descripcion, valor) {
 // // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto,
+  listarGastos,
+  anyadirGasto,
+  borrarGasto,
+  calcularTotalGastos,
+  calcularBalance,
+};
