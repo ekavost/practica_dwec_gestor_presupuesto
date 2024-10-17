112bbd1508133e84287eef9fe11dea2aef15467e
Ekaterina Vostrikova <ekavos@alu.edu.gva.es>
2024-10-17 18:16:39 +0200

    Fundamentos de JS 1: Crear objeto, modificar constructor

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,37 +1,34 @@
-// TODO: Crear las funciones, objetos
-
 let presupuesto = 0;
 
-function actualizarPresupuesto(valor) {
-  if (typeof valor === "number" && valor > 0) {
-    return (presupuesto = valor);
-  } else {
-    alert("Valor no válida");
+let comprobarPositivo = (numero) => typeof numero === "number" && numero >= 0;
+
+let actualizarPresupuesto = (valor) => {
+  if (comprobarPositivo(valor) == true) return (presupuesto = valor);
+  else {
+    console.log("Valor no válida");
     return -1;
   }
+};
+
+let mostrarPresupuesto = () => `Tu presupuesto actual es de ${presupuesto} €`;
+
+function CrearGasto(descripcion, valor) {
+  this.descripcion = descripcion;
+  comprobarPositivo(valor) ? (this.valor = valor) : (this.valor = 0);
+
+  this.mostrarGasto = function () {
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+  };
+
+  this.actualizarDescripcion = function (newDescripcion) {
+    return (this.descripcion = newDescripcion);
+  };
+  this.actualizarValor = function (newValor) {
+    if (comprobarPositivo(newValor)) return (this.valor = newValor);
+  };
 }
 
-function mostrarPresupuesto() {
-  return `Tu presupuesto actual es de ${presupuesto} €`;
-}
-
-function CrearGasto(valor) {
-  if (typeof valor === "number" && valor >= 0) {
-    this.valor = valor;
-  } else {
-    this.valor = 0;
-  }
-}
-
-function mostrarPresupuesto() {
-  // TODO
-}
-
-function CrearGasto() {
-  // TODO
-}
-
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+// // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
+// // Las funciones y objetos deben tener los nombres que se indican en el enunciado
+// // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
