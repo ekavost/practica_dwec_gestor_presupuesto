29837fe10b46b4261df5536b96624a5b9adb57a6
Ekaterina Vostrikova <ekavos@alu.edu.gva.es>
2024-10-27 10:31:49 +0100

    Fundamentos de JS III - modificar filtrarGastos, modificar propiedad etiquetas en CrearGast

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,8 +4,7 @@ let presupuesto = 0;
 let gastos = [];
 let idGasto = 0;
 
-let comprobarPositivo = (numero) =>
-  Number.isFinite(numero) && numero >= 0 ? numero : false;
+let comprobarPositivo = (numero) => (Number.isFinite(numero) && numero >= 0 ? numero : false);
 let comprobarFecha = (fecha) => Date.parse(fecha);
 
 function actualizarPresupuesto(valor) {
@@ -40,35 +39,33 @@ function calcularTotalGastos() {
 function calcularBalance() {
   return presupuesto - calcularTotalGastos();
 }
-//TODO
+
 function filtrarGastos(filtros) {
   return gastos.filter(function (gasto) {
     let result = true;
-    if (filtros.fechaDesde) {
-      result &&= gasto.fecha >= comprobarFecha(filtros.fechaDesde);
+    if (comprobarFecha(filtros.fechaDesde) && result) {
+      result = gasto.fecha >= comprobarFecha(filtros.fechaDesde);
     }
-    if (filtros.fechaHasta) {
-      result &&= gasto.fecha <= Date.parse(filtros.fechaHasta);
+    if (comprobarFecha(filtros.fechaHasta) && result) {
+      result = gasto.fecha <= comprobarFecha(filtros.fechaHasta);
     }
-    if (filtros.valorMinimo) {
-      result &&= gasto.valor >= filtros.valorMinimo;
+    if (filtros.valorMinimo && result) {
+      result = gasto.valor >= filtros.valorMinimo;
     }
-    if (filtros.valorMaximo) {
-      result &&= gasto.valor <= filtros.valorMaximo;
+    if (filtros.valorMaximo && result) {
+      result = gasto.valor <= filtros.valorMaximo;
     }
-    if (filtros.descripcionContiene) {
-      result &&= gasto.descripcion
-        .toLowerCase()
-        .includes(filtros.descripcionContiene.toLowerCase());
+    if (filtros.descripcionContiene && result) {
+      result = gasto.descripcion.toLowerCase().includes(filtros.descripcionContiene.toLowerCase());
     }
-    if (filtros.etiquetasTiene) {
+    if (filtros.etiquetasTiene && result) {
       let resultAux = false;
-      for (const etiqueta of filtros.etiquetasTiene) {
-        if (gasto.etiquetas.indexOf(etiqueta) > -1) {
+      filtros.etiquetasTiene.forEach((etiqueta) => {
+        if (gasto.etiquetas.includes(etiqueta.toLowerCase())) {
           resultAux = true;
         }
-      }
-      result &&= resultAux;
+      });
+      result = resultAux;
     }
     return result;
   });
@@ -81,22 +78,17 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
   this.descripcion = descripcion;
   this.valor = comprobarPositivo(valor) || 0;
   this.fecha = comprobarFecha(fecha) || new Date();
-  this.etiquetas = [...etiquetas];
+  this.etiquetas = [...etiquetas].map((etiqueta) => etiqueta.toLowerCase());
 
   this.mostrarGasto = function () {
     return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
   };
 
   this.mostrarGastoCompleto = function () {
-    let listaEtiquetas = this.etiquetas.reduce(
-      (output, etiqueta) => output.concat("- ", etiqueta, "\n"),
-      ""
-    );
+    let listaEtiquetas = this.etiquetas.reduce((output, etiqueta) => output.concat("- ", etiqueta, "\n"), "");
     let fechaAux = new Date(this.fecha);
 
-    return `Gasto correspondiente a ${this.descripcion} con valor ${
-      this.valor
-    } €.\nFecha: ${fechaAux.toLocaleString()}\nEtiquetas:\n${listaEtiquetas}`;
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${fechaAux.toLocaleString()}\nEtiquetas:\n${listaEtiquetas}`;
   };
 
   this.actualizarFecha = function (newFecha) {
@@ -113,15 +105,13 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
 
   this.anyadirEtiquetas = function (...newEtiquetas) {
     for (let etiqueta of newEtiquetas)
-      if (!this.etiquetas.includes(etiqueta)) {
-        this.etiquetas.push(etiqueta);
+      if (!this.etiquetas.includes(etiqueta.toLowerCase())) {
+        this.etiquetas.push(etiqueta.toLowerCase());
       }
   };
 
   this.borrarEtiquetas = function (...etiquetasToRemove) {
-    this.etiquetas = this.etiquetas.filter(
-      (etiqueta) => !etiquetasToRemove.includes(etiqueta)
-    );
+    this.etiquetas = this.etiquetas.filter((etiqueta) => !etiquetasToRemove.includes(etiqueta));
   };
 
   this.obtenerPeriodoAgrupacion = function (periodo) {
@@ -146,15 +136,4 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
 // // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export {
-  mostrarPresupuesto,
-  actualizarPresupuesto,
-  CrearGasto,
-  listarGastos,
-  anyadirGasto,
-  borrarGasto,
-  calcularTotalGastos,
-  calcularBalance,
-  filtrarGastos,
-  agruparGastos,
-};
+export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto, listarGastos, anyadirGasto, borrarGasto, calcularTotalGastos, calcularBalance, filtrarGastos, agruparGastos };
