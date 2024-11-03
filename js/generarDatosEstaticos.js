b28e2def1e4dbee33eb85294af83ac4ac26ccd49
Ekaterina Vostrikova <ekavos@alu.edu.gva.es>
2024-11-03 13:25:12 +0100

    Interacción de JS con HTML: Función mostrarGastoWeb; Empezar fichero generarDatosEstaticos; Enlazar script

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,33 @@
+import {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto,
+  listarGastos,
+  anyadirGasto,
+  borrarGasto,
+  calcularTotalGastos,
+  calcularBalance,
+  filtrarGastos,
+  agruparGastos,
+} from "./gestionPresupuesto.js";
+
+import { mostrarDatoEnId, mostrarGastoWeb } from "./gestionPresupuestoWeb.js";
+let newPresupuesto = actualizarPresupuesto(1500);
+let stringPresupuesto = mostrarPresupuesto(newPresupuesto);
+mostrarDatoEnId(stringPresupuesto, "presupuesto");
+let gasto1 = new CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new CrearGasto("Bonobús", 18.6, "2020-05-26", "transporte");
+let gasto4 = new CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+anyadirGasto(gasto1);
+anyadirGasto(gasto2);
+anyadirGasto(gasto3);
+anyadirGasto(gasto4);
+anyadirGasto(gasto5);
+anyadirGasto(gasto6);
+
+let totalGastos = calcularTotalGastos();
+mostrarDatoEnId(totalGastos, "gastos-totales");
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,6 +1,39 @@
 function mostrarDatoEnId(valor, idElemento) {
-  let id = document.getElementById(idElemento);
-  id.innerHTML = valor;
+  let doc = document.getElementById(idElemento);
+  doc.innerHTML = valor;
 }
-function mostrarGastoWeb(idElemento, gasto) {}
+function mostrarGastoWeb(idElemento, gasto) {
+  let doc = document.getElementById(idElemento);
+
+  let divGasto = document.createElement("div");
+  divGasto.className = "gasto";
+  doc.append(divGasto);
+
+  let divDescripcion = document.createElement("div");
+  divDescripcion.className = "gasto-descripcion";
+  divDescripcion.innerHTML = gasto.descripcion;
+  divGasto.append(divDescripcion);
+
+  let divFecha = document.createElement("div");
+  divFecha.className = "gasto-fecha";
+  divFecha.innerHTML = new Date(gasto.fecha).toLocaleDateString();
+  divDescripcion.append(divFecha);
+
+  let divValor = document.createElement("div");
+  divValor.className = "gasto-valor";
+  divValor.innerHTML = gasto.valor;
+  divFecha.append(divValor);
+
+  let divEtiquetas = document.createElement("div");
+  divEtiquetas.className = "gasto-etiquetas";
+  for (const etiqueta of gasto.etiquetas) {
+    let spanEtiqueta = document.createElement("span");
+    spanEtiqueta.className = "gasto-etiquetas-etiqueta";
+    spanEtiqueta.innerHTML = etiqueta + " ";
+    divEtiquetas.append(spanEtiqueta);
+  }
+  divValor.append(divEtiquetas);
+}
 function mostrarGastosAgrupadosWeb(idElenento, agrup, periodo) {}
+
+export { mostrarDatoEnId, mostrarGastoWeb };
